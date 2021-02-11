4355547cb18cbb10188ca2f93ee82d641d17e859
lorenzo-pc <lorenzo.ferrari@lsgalilei.org>
2021-02-11 19:48:27 +0100

    implementato in modo brutto

---
+++ labyrinth.cpp
@@ -0,0 +1,2 @@
+#include "labyrinth.h"
+
---
+++ labyrinth.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include <iomanip>
+#include <iostream>
+
+void print(std::vector<std::vector<char>> &labyrinth);
+void generate(std::vector<std::vector<char>> &labyrinth);
