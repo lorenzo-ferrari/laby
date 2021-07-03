d6422acdbade8de50e08b67d1ebe23b80b10ee69
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2021-07-03 23:06:38 +0200

    updated constants.h

--- src/constants.h
+++ src/constants.h
@@ -1,20 +1,8 @@
 // ROWS and COLUMNS must be odd
-#ifndef ROWS_DEFINED
-#define ROWS_DEFINED
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 inline const int ROWS = 69; // c++17
-#endif
-
-#ifndef COLUMNS_DEFINED
-#define COLUMNS_DEFINED
 inline const int COLUMNS = 69; // c++17
-#endif
-
-#ifndef EMPTY_DEFINED
-#define EMPTY_DEFINED
 inline const char EMPTY = ' '; // c++17
-#endif
-
-#ifndef WALL_DEFINED
-#define WALL_DEFINED
 inline const char WALL = '#'; // c++17
 #endif
