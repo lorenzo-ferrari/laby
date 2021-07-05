f45d2cb079c1d0346f003dedf7c73ea614085f1f
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2021-07-05 19:09:57 +0200

    headers

--- include/constants.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-inline const int ROWS = 11; // c++17
-inline const int COLUMNS = 33; // c++17
-inline const char EMPTY = ' '; // c++17
-inline const char WALL = '#'; // c++17
-#endif
--- include/labyrinth.h
+++ include/labyrinth.h
@@ -1,21 +1,42 @@
 #ifndef LABYRINTH_H
 #define LABYRINTH_H
 #include <algorithm>
+#include <array>
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include <ctime>
 
-#include "constants.h"
+namespace laby
+{
+#ifdef CLI_ARGUMENTS
+    constexpr char WELCOME_FILE[30] { "msg/welcome.txt" };
+    constexpr char HELP_FILE[30] { "msg/help.txt" };
+    constexpr char UNKNOWN_OPTION[30] { "msg/unknown.txt" };
+#endif
 
-struct Laby_t {
-    std::vector<std::vector<char>> plan;
-    std::vector<std::vector<bool>> reachable;
+    constexpr bool EMPTY { 0 };
+    constexpr bool WALL  { 1 };
+    
+    constexpr std::pair<int, int> UP    { -2,  0 };
+    constexpr std::pair<int, int> DOWN  {  2,  0 };
+    constexpr std::pair<int, int> LEFT  {  0,  2 };
+    constexpr std::pair<int, int> RIGHT {  0, -2 };
+    
+    constexpr std::array<std::pair<int, int>, 4> dirs { UP, DOWN, LEFT, RIGHT };
+}
 
-    Laby_t(int, int);
+class laby_t {
+public:
+    const int rows;
+    const int cols;
+    std::vector<std::vector<bool>> map;
+    std::vector<std::vector<bool>> vis;
+    std::vector<std::pair<int, int>> dirs;
+
+    laby_t(int, int);
+    bool valid(int, int);
+    void dfs(int, int);
+    void print(std::ostream&);
+    void print_bmp(std::ostream&);
 };
-
-void generate(int x, int y, Laby_t &labyrinth);
-void printLabyrinth(Laby_t &labyrinth);
-void initLabyrinth(Laby_t &labyrinth);
-bool checkForErrors();
 #endif
